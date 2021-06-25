b31721d2b027a9f2deb4684ffc608b5fe420a6a5
klepa1234567 <klepa1234567@bk.ru>
2021-06-25 16:26:32 +0300

    Name

--- src/index.js
+++ src/index.js
@@ -4,31 +4,16 @@ import './styles.css';
 
 
 const rootElement = document.querySelector('#root');
-const renderSeconds = time => {
-    const seconds = new Date(time).getSeconds();
-    const backgroundColor = seconds % 2 === 0
-        ? '#fff'
-        : '#000';
-
-    const textColor = seconds % 2 !== 0
-        ? '#fff'
-        : '#000';
-
-    const styles = {
-        color: textColor,
-        backgroundColor
-    }
-
-    const element = (
-        <div
-            className="seconds"
-            style={styles}
-        >
-            {seconds}
+const SearchField = (props) => {
+    return (
+    <div className="search">
+        <h1 className="search_title">{`Hello, ${props.name}. What to search for you?`}</h1>
+        <div className="search_field">
+            <input type="text" className="search_input"/>
+            <button className="search_button">Search</button>
         </div>
+    </div>
     );
-    ReactDOM.render(element, rootElement);
-}
-renderSeconds(new Date());
+};
+    ReactDOM.render(<SearchField name="Bob"/>, rootElement);
 
-setInterval(() => renderSeconds(new Date()), 1000);
