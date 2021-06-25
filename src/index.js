1b045618c25f1056c7ae1357a9319f2ccc3f6ee6
klepa1234567 <klepa1234567@bk.ru>
2021-06-25 17:39:27 +0300

    NameClass

--- src/index.js
+++ src/index.js
@@ -1,19 +1,21 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import './styles.css';
 
 
 const rootElement = document.querySelector('#root');
-const SearchField = (props) => {
-    return (
+class SearchField extends Component {
+    render (){
+        return(
     <div className="search">
-        <h1 className="search_title">{`Hello, ${props.name}. What to search for you?`}</h1>
+        <h1 className="search_title">{`Hello, ${this.props.name}. What to search for you?`}</h1>
         <div className="search_field">
             <input type="text" className="search_input"/>
             <button className="search_button">Search</button>
         </div>
     </div>
     );
+    }
 };
     ReactDOM.render(<SearchField name="Bob"/>, rootElement);
 
